37343507ab09c61f1ab90fa28369aa75865209c7
Ernesto Andres Rincon Cruz <ernesto.arc@gmail.com>
2022-11-12 07:24:41 -0500

    Ajustes a código fuente de proyecto principal

--- K32L2B31A_labs_diplomado_iot/source/K32L2B31A_labs_diplomado_iot.c
+++ K32L2B31A_labs_diplomado_iot/source/K32L2B31A_labs_diplomado_iot.c
@@ -1,72 +1,83 @@
-/*
- * Copyright 2016-2022 NXP
- * All rights reserved.
+/*!@file : K32L2B31A_labs_diplomado_iot.c
+ * @author  Ernesto Andres Rincon Cruz
+ * @version 0.00
+ * @date    05/10/2022
+ * @brief   Funcion principal main
+ * @details
+ *			v0.000	Proyecto base creado usando MCUXpresso
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
  *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of NXP Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
-/**
- * @file    K32L2B31A_labs_diplomado_iot.c
- * @brief   Application entry point.
- */
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
 #include <stdio.h>
 #include "board.h"
 #include "peripherals.h"
 #include "pin_mux.h"
 #include "clock_config.h"
 #include "K32L2B31A.h"
+
 #include "fsl_debug_console.h"
-/* TODO: insert other include files here. */
+#include "fsl_adc16.h"
+#include "lpuart0.h"
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
 
-/* TODO: insert other definitions and declarations here. */
+/*******************************************************************************
+ * Private Prototypes
+ ******************************************************************************/
 
-/*
- * @brief   Application entry point.
- */
+/*******************************************************************************
+ * External vars
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Local vars
+ ******************************************************************************/
+/* Force the counter to be placed into memory. */
+volatile static uint8_t i = 0 ;
+uint16_t dato_adc;
+/*******************************************************************************
+ * Private Source Code
+ ******************************************************************************/
 int main(void) {
-
-    /* Init board hardware. */
-    BOARD_InitBootPins();
-    BOARD_InitBootClocks();
-    BOARD_InitBootPeripherals();
+	/* Init board hardware. */
+	BOARD_InitBootPins();
+	BOARD_InitBootClocks();
+	BOARD_InitBootPeripherals();
 #ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
-    /* Init FSL debug console. */
-    BOARD_InitDebugConsole();
+	/* Init FSL debug console. */
+	BOARD_InitDebugConsole();
 #endif
 
-    PRINTF("Hello World 1\r\n");
+	/*Configurar canal ADC por donde se desea lectura
+	 * dar señal de start al ADC */
+	ADC16_SetChannelConfig(ADC0_PERIPHERAL, ADC0_CH0_CONTROL_GROUP,	&ADC0_channelsConfig[0]);
 
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
-    while(1) {
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-            tight while() loop */
-        __asm volatile ("nop");
-    }
-    return 0 ;
+	/*Esperar que el ADC finalice el ADC*/
+	while (0U == (kADC16_ChannelConversionDoneFlag	& ADC16_GetChannelStatusFlags(ADC0_PERIPHERAL,	ADC0_CH0_CONTROL_GROUP))) {
+	}
+
+	dato_adc=ADC16_GetChannelConversionValue(ADC0_PERIPHERAL,ADC0_CH0_CONTROL_GROUP);
+	/*Captura dato ADC e imprime por consola*/
+	PRINTF("ADC Value: %d\r\n",	dato_adc);
+
+	/* Enter an infinite loop, just incrementing a counter. */
+	while (1) {
+		i++;
+		if(leer_bandera_nuevo_dato()!=0){
+			escribir_bandera_nuevo_dato(0);
+			PRINTF("ADC Value: %c\r\n",	leer_dato());
+		}
+		/* 'Dummy' NOP to allow source level single stepping of
+		 tight while() loop */
+		__asm volatile ("nop");
+	}
+	return 0;
 }
+
+
+
